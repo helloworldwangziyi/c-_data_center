d6fa7d95ff38908c4a5cc506d5fb2a9ba319ff16
wangziyi <2389794787@qq.com>
2023-03-26 15:13:08 +0800

    提交4 5版本的crtsurfdata.cpp 加上了对xml json csv 文件的支持

--- idc1/c/crtsurfdata.cpp
+++ idc1/c/crtsurfdata.cpp
@@ -38,22 +38,28 @@ struct st_surfdata
 
 vector<struct st_surfdata> vsurfdata;  // 存放全国气象站点分钟观测数据的容器
 
+char strddatetime[21]; // 观测数据的时间。
+
 // 模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器中。
 void CrtSurfData();
+
+// 把容器vsurfdata中的全国气象站点分钟观测数据写入文件。
+bool CrtSurfFile(const char *outpath,const char *datafmt);
 
 CLogFile logfile;    // 日志类。
 
 int main(int argc,char *argv[])
 {
-  if (argc!=4) 
+  if (argc!=5) 
   {
     // 如果参数非法，给出帮助文档。
-    printf("Using:./crtsurfdata inifile outpath logfile\n");
-    printf("Example:/project/idc1/bin/crtsurfdata /project/idc1/ini/stcode.ini /tmp/surfdata /log/idc/crtsurfdata.log\n\n");
+    printf("Using:./crtsurfdata inifile outpath logfile datafmt\n");
+    printf("Example:/project/idc1/bin/crtsurfdata /project/idc1/ini/stcode.ini /tmp/idc/surfdata /log/idc/crtsurfdata.log xml,json,csv\n\n");
 
     printf("inifile 全国气象站点参数文件名。\n");
     printf("outpath 全国气象站点数据文件存放的目录。\n");
-    printf("logfile 本程序运行的日志文件名。\n\n");
+    printf("logfile 本程序运行的日志文件名。\n");
+    printf("datafmt 生成数据文件的格式，支持xml、json和csv三种格式，中间用逗号分隔。\n\n");
 
     return -1;
   }
@@ -72,6 +78,11 @@ int main(int argc,char *argv[])
 
   // 模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器中。
   CrtSurfData();
+
+  // 把容器vsurfdata中的全国气象站点分钟观测数据写入文件。
+  if (strstr(argv[4],"xml")!=0) CrtSurfFile(argv[2],"xml");
+  if (strstr(argv[4],"json")!=0) CrtSurfFile(argv[2],"json");
+  if (strstr(argv[4],"csv")!=0) CrtSurfFile(argv[2],"csv");
 
   logfile.WriteEx("crtsurfdata 运行结束。\n");
 
@@ -136,7 +147,6 @@ void CrtSurfData()
   srand(time(0));
 
   // 获取当前时间，当作观测时间。
-  char strddatetime[21];
   memset(strddatetime,0,sizeof(strddatetime));
   LocalTime(strddatetime,"yyyymmddhh24miss");
 
@@ -163,3 +173,55 @@ void CrtSurfData()
   }
 }
 
+// 把容器vsurfdata中的全国气象站点分钟观测数据写入文件。
+bool CrtSurfFile(const char *outpath,const char *datafmt)
+{
+  CFile File;
+
+  // 拼接生成数据的文件名，例如：/tmp/idc/surfdata/SURF_ZH_20210629092200_2254.csv
+  char strFileName[301];
+  sprintf(strFileName,"%s/SURF_ZH_%s_%d.%s",outpath,strddatetime,getpid(),datafmt);
+
+  // 打开文件。
+  if (File.OpenForRename(strFileName,"w")==false)
+  {
+    logfile.Write("File.OpenForRename(%s) failed.\n",strFileName); return false;
+  }
+
+  // 写入第一行标题。
+  if (strcmp(datafmt,"csv")==0) File.Fprintf("站点代码,数据时间,气温,气压,相对湿度,风向,风速,降雨量,能见度\n");
+
+  // 遍历存放观测数据的vsurfdata容器。
+  for (int ii=0;ii<vsurfdata.size();ii++)
+  {
+    // 写入一条记录。
+    if (strcmp(datafmt,"csv")==0)
+      File.Fprintf("%s,%s,%.1f,%.1f,%d,%d,%.1f,%.1f,%.1f\n",\
+         vsurfdata[ii].obtid,vsurfdata[ii].ddatetime,vsurfdata[ii].t/10.0,vsurfdata[ii].p/10.0,\
+         vsurfdata[ii].u,vsurfdata[ii].wd,vsurfdata[ii].wf/10.0,vsurfdata[ii].r/10.0,vsurfdata[ii].vis/10.0);
+
+    if (strcmp(datafmt,"xml")==0)
+      File.Fprintf("<obtid>%s</obtid><ddatetime>%s</ddatetime><t>%.1f</t><p>%.1f</p>"\
+                   "<u>%d</u><wd>%d</wd><wf>%.1f</wf><r>%.1f</r><vis>%.1f</vis><endl/>\n",\
+         vsurfdata[ii].obtid,vsurfdata[ii].ddatetime,vsurfdata[ii].t/10.0,vsurfdata[ii].p/10.0,\
+         vsurfdata[ii].u,vsurfdata[ii].wd,vsurfdata[ii].wf/10.0,vsurfdata[ii].r/10.0,vsurfdata[ii].vis/10.0);
+
+    if (strcmp(datafmt,"json")==0)
+      File.Fprintf("{\"obtid\":\"%s\",\"ddatetime\":\"%s\",\"t\":\"%.1f\",\"p\":\"%.1f\","\
+                   "\"u\":\"%d\",\"wd\":\"%d\",\"wf\":\"%.1f\",\"r\":\"%.1f\",\"vis\":\"%.1f\"}",\
+         vsurfdata[ii].obtid,vsurfdata[ii].ddatetime,vsurfdata[ii].t/10.0,vsurfdata[ii].p/10.0,\
+         vsurfdata[ii].u,vsurfdata[ii].wd,vsurfdata[ii].wf/10.0,vsurfdata[ii].r/10.0,vsurfdata[ii].vis/10.0);
+    if (ii<vsurfdata.size()-1) File.Fprintf(",\n");
+    else   File.Fprintf("\n");
+  }
+
+  if (strcmp(datafmt,"xml")==0) File.Fprintf("</data>\n");
+  if (strcmp(datafmt,"json")==0) File.Fprintf("]}\n");
+  
+  // 关闭文件。
+  File.CloseAndRename();
+
+  logfile.Write("生成数据文件%s成功，数据时间%s，记录数%d。\n",strFileName,strddatetime,vsurfdata.size());
+
+  return true;
+}
